f133b8bcaf79bf598c714039fa714fbd0bef2709
SHEN TUNG <vcd52088@gmail.com>
2026-03-15 23:33:46 +0900

    修正Cookie 問題

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import axios from 'axios';  // 加這行
+axios.defaults.withCredentials = true;  // 加這行
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
